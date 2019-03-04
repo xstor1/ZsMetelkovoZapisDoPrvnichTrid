735ccd2bed2f962ca9ad321ce8877165e52f8098
Patricie Vávrová <patavav@gmail.com>
2019-03-04 17:51:04 +0100

    Sourozenec puf

--- Database/Database.php
+++ Database/Database.php
@@ -4,7 +4,7 @@ class Database {
 const HOST = '127.0.0.1';
     const USER = 'root';
     const PASSWORD = '';
-    const DBNAME = 'larvasystemscz3';
+    const DBNAME = 'zapis';
 
     private $conn;
 
